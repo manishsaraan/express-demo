fdfe167497fb6d8515d7e54dfd2a94048bf31d20
manishsaraan <manish021js@gmail.com>
2017-10-21 00:25:22 +0530

    bower implemented

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express  = require('express');
 var app  = express();
 var PORT = process.env.PORT || 5000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 //starting routes
 app.get("/",function(req, res){
 	 res.send("Hello World!!!");
