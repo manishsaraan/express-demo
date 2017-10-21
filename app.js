12625edc8d2a51cd2992ab04799170f37c6efe97
manishsaraan <manish021js@gmail.com>
2017-10-21 20:07:37 +0530

    ejs templating engine added

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ var handlebars = require('express-handlebars');
 
 app.use(express.static('public'));
 app.set('views', './src/views');
+
+//code to set jade app engine
 app.set('view engine', 'jade'); 
 app.engine('.hbs', handlebars({extname : '.hbs'}));
+
+//code to set handlebars app engine
 app.set('view engine', '.hbs'); 
+
+//code to set ejs app engine
+app.set('view engine', 'ejs');
+
 
 //starting routes
 app.get('/',function(req, res){
-	 res.render('index',{title : 'Hello Handlebars', list: ['1','2','3','4']});
+	 res.render('index',{title : 'Hello from render', list: ['1','2','3','4']});
 });
 app.listen(PORT,function(err){
 	console.log('App is running at port : ' + PORT);
