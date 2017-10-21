77246040b6b386f84dd9294bc4862ec2b809025c
manishsaraan <manish021js@gmail.com>
2017-10-21 19:54:27 +0530

    handlebars templating engine added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express  = require('express');
 var app  = express();
 var PORT = process.env.PORT || 5000;
+var handlebars = require('express-handlebars');
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'jade'); 
+app.engine('.hbs', handlebars({extname : '.hbs'}));
+app.set('view engine', '.hbs'); 
 
 //starting routes
 app.get('/',function(req, res){
-	 res.render('index',{list: ['1','2','3','4']});
+	 res.render('index',{title : 'Hello Handlebars', list: ['1','2','3','4']});
 });
 app.listen(PORT,function(err){
 	console.log('App is running at port : ' + PORT);
