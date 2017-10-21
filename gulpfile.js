58a97e5a0a2ed21801645876c1b2e78b75b41b7a
manishsaraan <manish021js@gmail.com>
2017-10-21 19:04:51 +0530

    wiredep added

--- gulpfile.js
+++ gulpfile.js
@@ -11,3 +11,24 @@ gulp.task('style',function(){
          }))
          .pipe(jscs());
 });
+
+gulp.task('inject', function(){
+	 var wiredep = require('wiredep').stream;
+	 var inject = require('gulp-inject');
+	 var injectSrc = gulp.src(['./public/css/*.css',
+	 	                       'public/js/*.js'],{read : false});
+
+	 var injectOption = {
+	 	 ignorePath : '/public'
+	 };
+	 var options = {
+	 	 bowerJson : require('./bower.json'),
+	 	 directory : './public/lib',
+	 	 ignorePath : '../../public'
+	 }
+
+	 return gulp.src('./src/views/*.html')
+	            .pipe(wiredep(options))
+	            .pipe(inject(injectSrc,injectOption))
+	            .pipe(gulp.dest('./src/views'));
+});
